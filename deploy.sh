74aa854c5f31ff1d0f6206ff8547c0fa36c6c247
Salam Olanrewaju <salam_lanre@yahoo.com>
2018-10-12 19:35:00 +0100

    Added composer download

--- deploy.sh
+++ deploy.sh
@@ -128,7 +128,10 @@ fi
 # 5. Install Composer modules 
 if [ -e "$DEPLOYMENT_TARGET/composer.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval php composer install
+  echo "Downloading Composer"
+  curl -sS https://getcomposer.org/installer | php
+  #eval php composer install
+  eval php -d extension=php_intl.dll $DEPLOYMENT_TARGET/composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
   exitWithMessageOnError "composer failed"
   cd - > /dev/null
 fi
